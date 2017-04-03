ec9703b5daff9aa68741bb4099891311fd0da9a0
Wouter Beek <wouter@triply.cc>
2017-04-03 15:30:42 +0200

    CLEAN: Reordered meta tags in pack.nl alphabetically.

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
+author("Wouter Beek", 'wouter@triply.cc').
+download('https://github.com/wouterbeek/ckan_api/archive/V*.zip').
+keywords(["CKAN","Open Data","REST API"]).
 name(ckan_api).
 title("One API to access all CKAN sites.").
 version('0.0.1').
-keywords(["CKAN","Open Data","REST API"]).
-author("Wouter Beek", 'wouter@triply.cc').
-download('https://github.com/wouterbeek/ckan_api/archive/V*.zip').
