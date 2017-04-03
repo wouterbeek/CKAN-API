02108f258fd9511965073709c07e2d5bb4ba2ce5
Wouter Beek <wouter@triply.cc>
2017-04-03 11:32:40 +0200

    Initial commit.

---
+++ pack.pl
@@ -0,0 +1,6 @@
+name(ckan_api).
+title("One API to access all CKAN sites.").
+version('0.0.1').
+keywords(["CKAN","Open Data","REST API"]).
+author("Wouter Beek", 'wouter@triply.cc').
+download('https://github.com/wouterbeek/ckan_api/archive/V*.zip').
