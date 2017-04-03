8013c1f8d957cf067760c7bb3a9359be85053b21
Wouter Beek <wouter@triply.cc>
2017-04-03 19:35:21 +0200

    FIXED: Strings are not allowed in pack.pl

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
-author("Wouter Beek", 'wouter@triply.cc').
+author('Wouter Beek', 'wouter@triply.cc').
 download('https://github.com/wouterbeek/ckan_api/releases/*.zip').
-keywords(["CKAN","Open Data","REST API"]).
+keywords(['CKAN','Open Data','REST API']).
 name(ckan_api).
-title("One API to access all CKAN sites.").
+title('One API to access all CKAN sites.').
 version('0.0.1').
