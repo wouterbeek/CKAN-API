f84f5d7866870d9182ed56fe598fc631d2e2feea
Wouter Beek <wouter@triply.cc>
2017-04-03 19:29:33 +0200

    FIXED: Download URI (Github).

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 author("Wouter Beek", 'wouter@triply.cc').
-download('https://github.com/wouterbeek/ckan_api/archive/V*.zip').
+download('https://github.com/wouterbeek/ckan_api/releases/*.zip').
 keywords(["CKAN","Open Data","REST API"]).
 name(ckan_api).
 title("One API to access all CKAN sites.").
