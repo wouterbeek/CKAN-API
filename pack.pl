1c56da4e64ab1736eaa57004ac16bd6147778c37
Wouter Beek <wouter@triply.cc>
2018-07-17 21:18:05 +0200

    CHANGED: Use PPM.

--- pack.pl
+++
@@ -1,6 +0,0 @@
-author('Wouter Beek', 'wouter@triply.cc').
-download('https://github.com/wouterbeek/ckan_api/releases/tag/*.zip').
-keywords(['API','CKAN','HTTP','JSON','Open Data','REST']).
-name(ckan_api).
-title('Prolog-based CKAN API.').
-version('0.0.0').
